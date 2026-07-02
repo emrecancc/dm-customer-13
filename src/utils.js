c107cf4a348f30d4de0d97f3f006cd854ac20fbc
emrecancc <emrecancerli55@gmail.com>
2026-07-03 01:33:21 +0300

    ci: introduce syn_js (s96)

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,10 @@
-export const mapUser = (x) => {
-  return {
-    id: x.id,
-    name: x.name,
-  };
-};
+function transformItems(items) {
+  return items
+    .filter(x => x.active)
+    .map(x => ({
+      id: x.id
+      name: x.name,
+      value: x.value * 2,
+    }));
+}
+module.exports = { transformItems };
